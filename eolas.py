8bf51476afe70a43e718644ae5e62f5ca786bf05
Thomas Mahieux <thomas.mahieux@gmail.com>
2017-12-13 12:50:03 +0100

    Update eolas.py

--- eolas.py
+++ eolas.py
@@ -1,6 +1,8 @@
+import discord
 from discord.ext import commands
 
 description = '''There are a number of utility commands being showcased here.'''
+eolas = discord.Client()
 eolas = commands.Bot(command_prefix='?', description=description)
 
 token = 'YOUR_BOT_TOKEN'
@@ -14,23 +16,19 @@ extensions = (
 )
 
 
-def main():
-    @eolas.event
-    async def on_ready():
-        print('Logged in as')
-        print(eolas.user.name)
-        print(eolas.user.id)
-        print('------')
+@eolas.event
+async def on_ready():
+    print('Logged in as')
+    print(eolas.user.name)
+    print(eolas.user.id)
+    print('------')
 
-    for extension in extensions:
-        try:
-            eolas.load_extension(extension)
-        except Exception as e:
-            exc = '{}: {}'.format(type(e).__name__, e)
-            print('Failed to load extension {}\n{}'.format(extension, exc))
+for extension in extensions:
+    try:
+        eolas.load_extension(extension)
+    except Exception as e:
+        exc = '{}: {}'.format(type(e).__name__, e)
+        print('Failed to load extension {}\n{}'.format(extension, exc))
 
-    eolas.run(token)
+eolas.run(token)
 
-
-if __name__ == '__main__':
-    main()
